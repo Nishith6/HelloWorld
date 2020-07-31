38054f75c52f378c10df6ba9c90ca2f8cc535dcb
Nishith <58653204+Nishith6@users.noreply.github.com>
2020-07-31 14:32:51 +0530

    Updates

---
+++ precache-manifest.45ca084f9e8b9b148be2009156b865a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8e529d0468aa67f0f8b55fa17a9bd1",
+    "url": "/Covid-19_tracker/index.html"
+  },
+  {
+    "revision": "467d3fb502820603b9ba",
+    "url": "/Covid-19_tracker/static/css/2.b756e824.chunk.css"
+  },
+  {
+    "revision": "56d4b83b0fc6eef3d36c",
+    "url": "/Covid-19_tracker/static/css/main.56329d8f.chunk.css"
+  },
+  {
+    "revision": "467d3fb502820603b9ba",
+    "url": "/Covid-19_tracker/static/js/2.a03e93f7.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-19_tracker/static/js/2.a03e93f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d4b83b0fc6eef3d36c",
+    "url": "/Covid-19_tracker/static/js/main.80850a99.chunk.js"
+  },
+  {
+    "revision": "ba50f572851572d3db99",
+    "url": "/Covid-19_tracker/static/js/runtime-main.ac3d0744.js"
+  }
+]);
